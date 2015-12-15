d41e63c23cb24805886702a9d6dbedfc900544e2
mail@coskuntekin.com <mail@coskuntekin.com>
2015-12-15 15:18:53 +0800

    initial commit

---
+++ source/assets/javascripts/main.js
@@ -0,0 +1,16 @@
+//------------------------------------
+//	#boostrap js components (http://getbootstrap.com/javascript/)
+//------------------------------------
+
+//= require bootstrap/transition
+//= require bootstrap/modal
+//= require bootstrap/dropdown
+//= require bootstrap/scrollspy
+//= require bootstrap/tab
+//= require bootstrap/tooltip
+//= require bootstrap/popover
+//= require bootstrap/alert
+//= require bootstrap/button
+//= require bootstrap/collapse
+//= require bootstrap/carousel
+//= require bootstrap/affix
