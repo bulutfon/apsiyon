0d1172c73642cc28b763f4580aeba55ea111d662
mail@coskuntekin.com <mail@coskuntekin.com>
2015-12-31 16:08:23 +0800

    optimized js components

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -2,15 +2,4 @@
 //	#boostrap js components (http://getbootstrap.com/javascript/)
 //------------------------------------
 
-//= require bootstrap/transition
-//# //= require bootstrap/modal
-//# //= require bootstrap/dropdown
-//# //= require bootstrap/scrollspy
-//# //= require bootstrap/tab
-//# //= require bootstrap/tooltip
-//# //= require bootstrap/popover
-//# //= require bootstrap/alert
 //= require bootstrap/button
-//# //= require bootstrap/collapse
-//# //= require bootstrap/carousel
-//# //= require bootstrap/affix
